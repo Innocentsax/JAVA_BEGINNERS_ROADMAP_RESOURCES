8f2a4371979794d186249c11020bb39849449595
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-25 21:31:50 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -160,4 +160,9 @@ LOWER(last_name) || '@gmail' AS custom_email FROM customer
 SELECT title,rental_rate FROM film 
 WHERE rental_rate > (SELECT AVG(rental_rate) FROM film);
 
-
+SELECT film_id, title FROM film 
+WHERE film_id IN (SELECT inventory.film_id 
+				  FROM rental INNER JOIN inventory 
+				  ON inventory.inventory_id = rental.inventory_id 
+				  WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30')
+ORDER BY film_id;
