4ce29fe5bf30286988804ef52df9900bca27a177
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-25 23:08:00 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -209,3 +209,13 @@ GROUP BY facid ORDER BY SUM(slots);
 
 SELECT facid,SUM(slots) AS total_slots FROM cd.bookings 
 GROUP BY facid HAVING SUM(slots) > 1000 ORDER BY facid;
+
+SELECT cd.bookings.starttime, cd.facilities.name FROM cd.facilities
+INNER JOIN cd.bookings ON cd.facilities.facid = cd.bookings.facid
+WHERE cd.facilities.facid IN (0,1) 
+AND cd.bookings.starttime >= '2012-09-21' AND cd.bookings.starttime < '2012-09-22'
+ORDER BY cd.bookings.starttime;
+
+SELECT cd.bookings.starttime FROM cd.bookings
+INNER JOIN cd.members ON cd.members.memid = cd.bookings.memid
+WHERE cd.members.firstname = 'David' AND cd.members.surname = 'Farrell';
