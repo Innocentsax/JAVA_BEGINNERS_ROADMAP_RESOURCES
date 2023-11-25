7de658bc203cb0f4eb26f62e475a9b98a2bf91be
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-25 21:46:31 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -173,3 +173,6 @@ WHERE NOT EXISTS (SELECT * FROM payment as p
 
 SELECT title,length FROM film WHERE length = 117;
 
+SELECT f1.title, f2.title, f1.length FROM film AS f1
+INNER JOIN film as f2 ON f1.film_id != f2.film_id AND
+f1.length = f2.length
