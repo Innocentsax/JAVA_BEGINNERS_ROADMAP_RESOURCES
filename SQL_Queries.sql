cfd7238551aa9cb13772a5f534d6ce03bb375fc3
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-25 17:50:32 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -135,3 +135,7 @@ SELECT EXTRACT(YEAR FROM payment_date) AS next_year FROM payment;
 SELECT EXTRACT(MONTH FROM payment_date) AS pay_month FROM payment;
 
 SELECT EXTRACT (QUARTER FROM payment_date) AS per_month FROM payment;
+
+SELECT AGE(payment_date) FROM payment;
+
+SELECT TO_CHAR(payment_date, 'dd/MM/YYYY') FROM payment;
