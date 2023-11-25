56edc0c047af6180799d18811e4b39e5316a95fe
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-25 21:32:32 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -166,3 +166,7 @@ WHERE film_id IN (SELECT inventory.film_id
 				  ON inventory.inventory_id = rental.inventory_id 
 				  WHERE return_date BETWEEN '2005-05-29' AND '2005-05-30')
 ORDER BY film_id;
+
+SELECT first_name, last_name FROM customer AS c 
+WHERE NOT EXISTS (SELECT * FROM payment as p 
+				  WHERE p.customer_id = c.customer_id AND amount > 11);
