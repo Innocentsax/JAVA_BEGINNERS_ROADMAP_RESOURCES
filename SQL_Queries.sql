0adb3255d315fc2435ea8662825492095a3bc25b
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-25 17:14:10 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -117,3 +117,5 @@ FROM film_actor INNER JOIN actor
 ON film_actor.actor_id = actor.actor_id INNER JOIN film 
 ON film_actor.film_id = film.film_id 
 WHERE first_name = 'Nick' AND last_name = 'Wahlberg';
+
+SHOW ALL
