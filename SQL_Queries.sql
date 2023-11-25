6aa02d9d180823766e37f460d0b7f2e9931b51b3
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-25 21:03:42 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -153,3 +153,6 @@ SELECT LENGTH(first_name) FROM customer;
 SELECT first_name || '  ' || last_name AS full_name FROM customer;
 
 SELECT upper(first_name) || '  ' || upper(last_name) AS full_name FROM customer;
+
+SELECT LOWER(LEFT(first_name,1)) || 
+LOWER(last_name) || '@gmail' AS custom_email FROM customer
