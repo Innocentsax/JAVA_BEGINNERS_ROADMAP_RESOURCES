caec8ceeb92ebda0c7bba89061c5b457efcc3b66
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-26 13:31:36 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -233,3 +233,9 @@ CREATE TABLE job(
 	job_id SERIAL PRIMARY KEY,
 	job_name VARCHAR(200) UNIQUE NOT NULL
 )
+
+CREATE TABLE account_job(
+	user_id INTEGER REFERENCES account(user_id),
+	job_id INTEGER REFERENCES job(job_id),
+	hire_date TIMESTAMP
+)
