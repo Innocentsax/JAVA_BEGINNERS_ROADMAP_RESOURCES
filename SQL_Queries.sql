15944b688c2506aa1dab5ab87609ed3ae9275793
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-26 17:02:39 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -372,3 +372,10 @@ SELECT (
 	SUM(CASE WHEN department = 'B' THEN 1 ELSE 0 END)
 ) AS department_ratio
 FROM depts
+
+
+SELECT (
+	SUM(CASE WHEN department = 'A' THEN 1 ELSE 0 END)/
+	NULLIF(SUM(CASE WHEN department = 'B' THEN 1 ELSE 0 END),0)
+) AS department_ratio
+FROM depts;
