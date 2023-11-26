862f045c177149eb42ae3ec562c1dc0f5a3abf7b
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-26 16:53:15 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -322,3 +322,10 @@ VALUES('Mark','Udo',5,'08163093928',2035)
 INSERT INTO teachers(first_name,last_name,
 					 homeroom_number,department,email,phone)
 VALUES('Innocent','Udo',9,'Software','udo@zenbyzenith','08163093928')
+
+SELECT customer_id, CASE
+	WHEN (customer_id <= 100) THEN 'Premium'
+	WHEN (customer_id BETWEEN 100 AND 200) THEN 'Plus'
+	ELSE 'Normal'
+END AS customer_class
+FROM customer;
