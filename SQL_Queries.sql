07a1f0505edce334545bf3d7384d5343bb94ee09
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-11-26 17:03:39 +0100

    Update SQL_Queries.sql

--- SQL_Queries.sql
+++ SQL_Queries.sql
@@ -384,3 +384,8 @@ FROM depts;
 CREATE VIEW customer_info AS 
 SELECT first_name,last_name,address FROM customer 
 INNER JOIN address ON customer.address_id = address.address_id;
+
+
+CREATE OR REPLACE VIEW customer_info AS 
+SELECT first_name,last_name,address, district FROM customer 
+INNER JOIN address ON customer.address_id = address.address_id;
